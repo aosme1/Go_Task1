cf3cbcf42123747e095847597a6b2c0ad4f7d7dc
aosme1 <abu_sunday@yahoo.co.uk>
2020-03-26 10:57:53 +0100

    commit

---
+++ dectobin.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x, y, z int = 30, 42, 53
+
+	fmt.Printf("x = %d, in binary format =%b\n", x, x)
+	fmt.Printf("y = %d, in binary format =%b\n", y, y)
+	fmt.Printf("z = %d, in binary format =%b\n", z, z)
+}
